18ee10d50e5309311fa557b469f8980caf714cd6
Juntao Wu <wujuntaocn@outlook.com>
2021-11-01 11:50:59 +0800

    removed password.

--- setup.sql
+++ setup.sql
@@ -1,9 +1,4 @@
-
--- create the users for each database
-DROP USER IF EXISTS 'showcase_user'@'%';
-CREATE USER 'showcase_user'@'%' IDENTIFIED BY 'showcase_password';
+CREATE USER 'root'@'%' IDENTIFIED BY '';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 CREATE DATABASE IF NOT EXISTS showcase_axon_exception;
-GRANT ALL PRIVILEGES ON showcase_axon_exception.* TO 'showcase_user'@'%' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON mysql.proc TO 'showcase_user'@'%' WITH GRANT OPTION;
-
 FLUSH PRIVILEGES;
